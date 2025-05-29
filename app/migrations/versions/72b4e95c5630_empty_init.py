8301b2479d68d6131911db42e700ff930bac8144
Mweresa Esther Magoma <esthermweresa27@gmail.com>
2025-05-29 09:42:39 +0300

    ll

---
+++ app/migrations/versions/72b4e95c5630_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 72b4e95c5630
+Revises: 
+Create Date: 2025-05-29 07:49:26.370967
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '72b4e95c5630'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
